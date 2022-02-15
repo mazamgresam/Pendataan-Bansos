e800fbc8f76eb17b46a58f89457d879e545eb1d8
azadra527@gmail.com <azadra527@gmail.com>
2022-02-15 08:38:10 +0700

    add dashboard

--- pendataan-bansos/src/App.vue
+++ pendataan-bansos/src/App.vue
@@ -1,5 +1,43 @@
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div id="bg-image" class="bg-gray-100 min-h-screen pb-8">
+    <header
+      class="flex justify-between sticky top-0 bg-gray-200 px-10 py-4 md:px-16 lg:px-24"
+    >
+      <div class="w-28">
+        <img src="./assets/logo_jds.png" alt="logo JDS" />
+      </div>
+      <div class="flex gap-8 items-center">
+        <div class="flex gap-3 lg:gap-6">
+          <router-link
+            :to="{ name: 'Home' }"
+            class="lg:text-xl font-bold text-gray-600 opacity-60 hover:opacity-100 hover:text-gray-800"
+            :class="{
+              'opacity-100 text-gray-700 border-b-2 border-gray-700':
+                $route.name == 'Home',
+            }"
+            >Beranda</router-link
+          >
+          <router-link
+            :to="{ name: 'Form' }"
+            class="lg:text-xl font-bold text-gray-600 opacity-60 hover:opacity-100 hover:text-gray-800"
+            :class="{
+              'opacity-100 text-gray-700 border-b-2 border-gray-700':
+                $route.name == 'Form',
+            }"
+            >Verifikasi</router-link
+          >
+        </div>
+      </div>
+    </header>
+    <router-view></router-view>
+  </div>
 </template>
+<script>
+export default {};
+</script>
+<style scoped>
+#bg-image {
+  background-image: url("./assets/bg.png");
+  background-size: cover;
+}
+</style>
---
+++ pendataan-bansos/src/views/Home.vue
@@ -0,0 +1,51 @@
+<template>
+  <div
+    class="lg:flex lg:justify-between gap-8 items-center 2xl:w-3/4 pt-28 pb-6 px-16 mx-auto text-lg"
+  >
+    <p>
+      Selamat Datang !
+      <span
+        ><br />
+        <h1
+          class="bg-gradient-to-r from-green-300 via-yellow-300 to-blue-500 text-transparent bg-clip-text font-bold text-5xl py-4"
+        >
+          Sistem Pendataan Bantuan Sosial
+        </h1>
+        <p>
+          Komplek Panghegar adalah sebuah area di wilayah Kelurahan Cipadung
+          Kulon, Kecamatan Panyileukan, yang terdiri dari 9 Rukun Warga (RW) dan
+          13 Rukun Tetangga (RT). Guna memastikan bantuan tersalurkan kepada
+          orang yang tepat, proses verifikasi dapat dilakukan dengan mudah, dan
+          data yang masuk bersifat valid, dibutuhkan sejumlah data yang
+          dilaporkan oleh RW.
+        </p></span
+      >
+    </p>
+
+    <img
+      class="xl:h-72 lg:h-56 w-11/12 sm:w-7/12 mx-auto"
+      src="../assets/bantuan.png"
+      alt="bansos"
+    />
+  </div>
+  <div class="xl:w-1/6 mx-auto text-center">
+    <button
+      @click="move"
+      class="bg-gradient-to-r from-green-300 to-yellow-200 font-bold text-2xl rounded-lg px-4 py-2 text-blue-500 opacity-80 hover:opacity-100 border-2 hover:border-blue-500"
+    >
+      Verifikasi Disini
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    move() {
+      this.$router.push({
+        name: "Form",
+      });
+    },
+  },
+};
+</script>
