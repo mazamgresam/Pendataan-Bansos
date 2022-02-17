0c191c876a7841474457e348b820a95143242f19
azadra527@gmail.com <azadra527@gmail.com>
2022-02-17 09:03:17 +0700

    add nprogrss dan flashmessage

--- pendataan-bansos/src/main.js
+++ pendataan-bansos/src/main.js
@@ -1,6 +1,12 @@
-import { createApp } from 'vue'
+import { createApp, reactive } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './index.css'
+import 'nprogress/nprogress.css'
 
-createApp(App).use(router).mount('#app')
+const GStore = reactive({ flashMessage: ''})
+
+createApp(App)
+.use(router)
+.provide('GStore', GStore)
+.mount('#app')
